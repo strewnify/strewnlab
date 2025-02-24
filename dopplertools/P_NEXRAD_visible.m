38d730d8d7e0181c0e3089bd9f0dcc1d21d243b4
Jim Goodall <james.a.goodall@gmail.com>
2025-02-23 23:40:40 -0500

    NEXRAD

--- dopplertools/P_NEXRAD_visible.m
+++ dopplertools/P_NEXRAD_visible.m
@@ -6,14 +6,19 @@ function probability = P_NEXRAD_visible(slantRange_km, objAZ, objELEV, vNorth, v
 % See Also
 %   P_NEXRAD_detect, getNEXRAD
 
-    spinrate_deg_s = getNEXRAD('spinrate_deg_s',VCPmode);
+    spinrate_deg_s = getNEXRAD('spinrate_deg_s',VCPmode); % get reflectivity scan rate NEED to lookup ELEV... fix
     elevations = getNEXRAD('elevations',VCPmode);
-    n_sweeps = numel(elevations);     % Number of radar sweeps per volume scan
-    sweep_time = 360 / spinrate_deg_s;
-    volume_scan_time_s = sweep_time * n_sweeps;
+    %n_sweeps = numel(elevations);     % Number of radar sweeps per volume scan
+    sweep_time_s = 360 / spinrate_deg_s;
+    volume_scan_time_s = getNEXRAD('VCP_duration_s',VCPmode);
     
     residence_time_s = beam_residence_time(slantRange_km, objAZ, objELEV, vNorth, vEast, vDown);
     
-    probability = residence_time_s ./ volume_scan_time_s;
+    % Calculate the probability that the object will be scanned
+    % meaning that residence will occur during a scan at that elevation.  
+    % Probabilty cannot be greater than 1.  In
+    % statistics this is known as an interval overlap problem.
+    probability = min(1,(residence_time_s + sweep_time_s) ./ volume_scan_time_s);
+    
 end
 
