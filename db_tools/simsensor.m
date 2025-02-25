70aa10709d360c693ed6cd1c2931b4b1fa71e503
Jim Goodall <james.a.goodall@gmail.com>
2025-02-25 15:53:28 -0500

    simsensor

--- db_tools/simsensor.m
+++ db_tools/simsensor.m
@@ -1,4 +1,4 @@
-function [observations] = simsensor(entry_time, sim_time, projectile, sensor_db, station_list, observations)
+function [observations] = simsensor(entry_time, sim_time, projectile, sensor_db, station_data, observations)
 % SIMULATE_SENSORS Simulates observations of an object from a list of sensor stations.
 %
 %   obs_table = simulate_sensors(entry_time, sim_time, lat, lon, alt_m, mass_kg, diameter_m, frontal_area_m2, ablation, sensor_db, station_list, obs_table)
@@ -29,11 +29,11 @@ function [observations] = simsensor(entry_time, sim_time, projectile, sensor_db,
 current = 1; % Do not change, defines current value in timestep arrays
 
 % Initialize table if not provided
-if nargin < 12 || isempty(observations)
-    observations = table([], [], [], [], [], [], [], [], [], [], [], [], [], [], [], 'VariableNames', ...
+if nargin < 6 || isempty(observations)
+    observations = table([], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], 'VariableNames', ...
         {'rockID', 'ObservationTime', 'SimulationTime', 'Lat', 'Long', 'Alt_km', ...
-         'Mass_kg', 'Diameter_m', 'frontalarea_m2', 'ablation', 'StationID', ...
-         'observed_AZ', 'observed_ELEV', 'slantRange_m', 'P_detect'});
+         'Mass_kg', 'Diameter_m', 'frontalarea_m2', 'ablation', 'StationID', 'sensorMode' ...
+         'observed_AZ', 'observed_ELEV', 'slantRange_m', 'signal_delay_s', 'magnitude_dB', 'P_detect'});
 end
 
 % Compute the current observation time
@@ -42,6 +42,16 @@ obs_time = entry_time + seconds(sim_time);
 for n = 1:size(projectile,2)
     if projectile(n).inflight > 0
         
+        % Clear previous
+        clear observed_AZ
+        clear observed_ELEV
+        clear slantRange_m
+        clear slantRange_km
+        clear P_detect
+        clear signal_delay_s
+        clear StationID
+        clear numrows
+                
         % Get projectile data
         rockID = projectile(n).rockID;
         LAT = projectile(n).location(current,1);
@@ -56,21 +66,24 @@ for n = 1:size(projectile,2)
         vDown_mps = projectile(n).vDown_mps;
 
         % Loop through specified stations
-        for i = 1:numel(station_list)
+        for station_i = 1:size(station_data,1)
             % Find the index of the station in the table
-            station_idx = find(strcmp(sensor_db.StationID, station_list{i}), 1);
+            station_idx = find(strcmp(sensor_db.StationID, station_data.StationID{station_i}), 1);
 
             % Skip if station not found
             if isempty(station_idx)
                 continue;
             end
 
+            % Record the stationID data
+            StationID(station_i) = station_data.StationID(station_i);
+            
             % Convert geodetic coordinates to AER (Azimuth, Elevation, Range)
-            [observed_AZ, observed_ELEV, slantRange_m] = geodetic2aer(LAT, LONG, altitude_m, ...
+            [observed_AZ(station_i), observed_ELEV(station_i), slantRange_m(station_i)] = geodetic2aer(LAT, LONG, altitude_m, ...
                 sensor_db.LAT(station_idx), sensor_db.LONG(station_idx), sensor_db.Altitude_m(station_idx), ...
                 getPlanet('ellipsoid_m'));
 
-            slantRange_km = slantRange_m ./ 1000;
+            slantRange_km(station_i) = slantRange_m(station_i) ./ 1000;
 
             % Check Sensor Type
             % Camera, Doppler, Geostationary, or Seismic
@@ -88,43 +101,45 @@ for n = 1:size(projectile,2)
 
                     % Check visibility
                     if Az_min < Az_max
-                        az_visible = (Az_min <= observed_AZ) & (observed_AZ <= Az_max);
+                        az_visible = (Az_min <= observed_AZ(station_i)) & (observed_AZ(station_i) <= Az_max);
                     else
-                        az_visible = (observed_AZ >= Az_min) | (observed_AZ <= Az_max);
+                        az_visible = (observed_AZ(station_i) >= Az_min) | (observed_AZ(station_i) <= Az_max);
                     end
 
-                    visible = (observed_ELEV > 0) & az_visible & (El_min <= observed_ELEV) & (observed_ELEV <= El_max);
+                    visible = (observed_ELEV(station_i) > 0) & az_visible & (El_min <= observed_ELEV(station_i)) & (observed_ELEV(station_i) <= El_max);
 
-                    P_detect = double(ablation); % convert logical array to 0 or 1
+                    P_detect(station_i) = double(ablation); % convert logical array to 0 or 1
 
                     % Nearly zero, but light speed radar delay calculated for completeness
-                    signal_delay_s = slantRange_m ./ getConstant('c_mps'); 
+                    signal_delay_s(station_i) = slantRange_m(station_i) ./ getConstant('c_mps'); 
+                    
+                    magnitude_dB = NaN;
 
                 case 'Doppler'
                      % Default mode
-                    VCP_mode = 'VCP21';
-
+                    VCP_mode = station_data.sensorMode{station_i};
+                    
                     % Calculate the probability that the object will be visible to a radar sweep
-                    P_visible = P_NEXRAD_visible(slantRange_km,observed_AZ,observed_ELEV,vNorth_mps,vEast_mps,vDown_mps,VCP_mode);
+                    P_visible = P_NEXRAD_visible(slantRange_km(station_i),observed_AZ(station_i),observed_ELEV(station_i),vNorth_mps,vEast_mps,vDown_mps,VCP_mode);
 
                     % Calculate the probability that the object would be detected, if it was in the beam
-                    [P_NEXRAD, magnitude_dB] = P_NEXRAD_detect(frontalarea_m2, slantRange_km);
+                    [P_NEXRAD, magnitude_dB(station_i)] = P_NEXRAD_detect(frontalarea_m2, slantRange_km(station_i));
 
                     % Combine the probabilities to get overall probability of detection
-                    P_detect = P_visible .* P_NEXRAD;
+                    P_detect(station_i) = P_visible .* P_NEXRAD;
 
                     % Nearly zero, but light speed radar delay calculated for completeness
-                    signal_delay_s = 2 .* slantRange_m ./ getConstant('c_mps'); 
+                    signal_delay_s(station_i) = 2 .* slantRange_m(station_i) ./ getConstant('c_mps'); 
 
                 case 'Geostationary'
                     % Not supported
                     % Once complete, this function will return probability of geostationary camera
                     % detection, based on ablation, camera FOV, and other factors
 
-                    P_detect = double(ablation); % convert logical array to 0 or 1
+                    P_detect(station_i) = double(ablation); % convert logical array to 0 or 1
 
                     % Nearly zero, but light speed radar delay calculated for completeness
-                    signal_delay_s = slantRange_m ./ getConstant('c_mps'); 
+                    signal_delay_s(station_i) = slantRange_m(station_i) ./ getConstant('c_mps'); 
 
                 case 'Seismic'
                     % Not supported
@@ -156,31 +171,31 @@ for n = 1:size(projectile,2)
                     % Calculate probability of sonic boom, based on sigmoid function, centered around mach 1
                     k = 0.13; % Estimated based on a transition range of ~30 m/s
                     P_Boom = 1 ./ (1 + exp(-k * (airspeed_mps - speedsound)));
-                    [P_Boom_detect, magnitude_dB] = P_Seismic_detect(slantRange_km, altitude_km, mass_kg, airspeed_mps);
+                    [P_Boom_detect, magnitude_dB] = P_Seismic_detect(slantRange_km(station_i), altitude_km, mass_kg, airspeed_mps);
 
                     Calculate the probability of detection, based on distance from the seismic station
-                    P_detect = P_Boom .* P_Boom_detect;
+                    P_detect(station_i) = P_Boom .* P_Boom_detect;
 
                     % Calculate sonic delay 
-                    signal_delay_s = slantRange_m ./ 333; 
+                    signal_delay_s(station_i) = slantRange_m(station_i) ./ 333; 
 
                 otherwise
                     logformat('Sensor type not supported', 'ERROR')
             end
-
-            % Count rows for repmat
-            numrows = size(altitude_m);
-
-            % Create the new rows for the observations table
-            new_rows = table( ...
-                repmat(rockID, numrows), repmat(obs_time, numrows), repmat(sim_time, numrows), LAT * ones(numrows), ...
-                LONG * ones(numrows), altitude_m / 1000, repmat(mass_kg, numrows), repmat(diameter_m, numrows), ...
-                repmat(frontalarea_m2, numrows), repmat(ablation, numrows), repmat({sensor_db.StationID{station_idx}}, numrows), ...
-                observed_AZ, observed_ELEV, slantRange_m, repmat(P_detect, numrows), 'VariableNames', observations.Properties.VariableNames);
-
-            % Append to the observations table
-            observations = [observations; new_rows];
         end
+        
+        % Count rows for repmat
+        numrows = numel(P_detect)';
+
+        % Create the new rows for the observations table
+        new_rows = table( ...
+            repmat(rockID, numrows,1), repmat(obs_time, numrows,1), repmat(sim_time, numrows,1), repmat(LAT, numrows,1), ...
+            repmat(LONG, numrows,1), repmat(altitude_m / 1000, numrows,1), repmat(mass_kg, numrows,1), repmat(diameter_m, numrows,1), ...
+            repmat(frontalarea_m2, numrows,1), repmat(ablation, numrows,1), StationID', ...
+            observed_AZ', observed_ELEV', slantRange_m', signal_delay_s', magnitude_dB', P_detect', 'VariableNames', observations.Properties.VariableNames);
+
+        % Append to the observations table
+        observations = [observations; new_rows];
     end
 end
 
--- generateRockID.m
+++ generateRockID.m
@@ -1,4 +1,5 @@
 function rockID = generateRockID()
-% Generates a random hash value to uniquely identify a simulated rock
-    rockID = string(DataHash(datenum(now)*rand));
+% Generates a random hash value to uniquely identify a simulated rock.
+
+    rockID = string(DataHash(datenum(now)*rand)); % Generate new rockID
 end
